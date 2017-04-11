6f43f932c790993d95a331bcd1a54482b57d764a
Derek Yu <yuderekyu@gmail.com>
2017-04-11 13:59:33 -0500

    Change fixing label endpoint

--- gateways/warehouse.go
+++ gateways/warehouse.go
@@ -21,7 +21,7 @@ type Warehouse interface {
 	GetAllOrders(offset int, limit int) ([]*models.Order, error)
 	NewOrder(newOrder *models.Order) (*models.Order, error)
 	GetOrderByID(id uuid.UUID) (*models.Order, error)
-	GetOrderLabel(id uuid.UUID) (string, error)
+	GetShippingLabel(newShipmentRequest *models.ShipmentRequest) (*string, error)
 	UpdateOrder(update *models.Order) (*models.Order, error)
 	DeleteOrder(id uuid.UUID) error
 	GetAllSubOrders(offset int, limit int) ([]*models.SubOrder, error)
@@ -146,16 +146,16 @@ func (w *warehouse) GetOrderByID(id uuid.UUID) (*models.Order, error) {
 	return &order, nil
 }
 
-func (w *warehouse) GetShippingLabel(newShipmentRequest *models.ShipmentRequest) (string, error) {
+func (w *warehouse) GetShippingLabel(newShipmentRequest *models.ShipmentRequest) (*string, error) {
 	url := fmt.Sprintf("%slabel", w.url)
 
-	var shipmentRequest models.ShipmentRequest
-	err := w.ServiceSend(http.MethodPost, url, newShipmentRequest, &shipmentRequest)
+	var order models.Order
+	err := w.ServiceSend(http.MethodPost, url, newShipmentRequest, &order.LabelURL)
 	if err != nil {
-		return "", err
+		return nil, err
 	}
 
-	return label, nil
+	return &order.LabelURL, nil
 }
 
 func (w *warehouse) UpdateOrder(update *models.Order) (*models.Order, error) {
